42a0007f131b97b0bb401e228989183f2f06e5a1
Talhamuhammadali <talha_muhammadali@hotmail.com>
2026-09-20 18:00:02 +0500

    [feat] asteroid field added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 print(f"Screen width: {SCREEN_WIDTH}")
@@ -17,9 +19,15 @@ def main():
     dt: float = 0.0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)    
+    
     while True:
         log_state()
         for event in pygame.event.get():
