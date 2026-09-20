70f8e44f791508af8f7594b41c4ccbe3876ed56b
Talhamuhammadali <talha_muhammadali@hotmail.com>
2026-09-20 17:23:16 +0500

    [feat] added movement to player object

--- player.py
+++ player.py
@@ -2,7 +2,7 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, LINE_WIDTH
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, LINE_WIDTH
 
 
 class Player(CircleShape):
@@ -30,6 +30,13 @@ class Player(CircleShape):
         """Rotate the player object"""
         self.rotation +=  PLAYER_TURN_SPEED * dt
     
+    def move(self, dt):
+        """Move the playe object."""
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+    
     def update(self, dt):
         """Move player object."""
         keys = pygame.key.get_pressed()
@@ -38,4 +45,8 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         
